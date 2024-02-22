190dec25b2473c3212693dfac8dc498f5de46d0f
Jain K <kuriakosejain456@gmail.com>
2024-02-22 23:00:22 +0530

    data fetch file

---
+++ src/components/QuizService.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API_URL = "https://opentdb.com/api.php";
+
+export interface Question {
+  question: string;
+  correct_answer: string;
+  incorrect_answer: string[];
+}
+
+export async function fetchQuestion(
+  category: number,
+  amount: number
+): Promise<Question[]> {
+  try {
+    const response = await axios.get(API_URL, {
+      params: {
+        amount,
+        category,
+        type: "multiple",
+      },
+    });
+
+    return response.data.results.map((result: any) => {
+      return {
+        question: result.question,
+        correct_answer: result.correct_answer,
+        incorrect_answer: result.incorrect_answer,
+      };
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw new Error("Failed to fetch");
+  }
+}

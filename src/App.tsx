1014ec6888341edc80ca505a18700f33177c5cd5
Jain K <kuriakosejain456@gmail.com>
2024-02-22 23:17:40 +0530

    importing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Question from "./components/Question";
+import Timer from "./components/Timer";
+import Score from "./components/Score";
+import {
+  fetchQuestion,
+  Question as QuestionType,
+} from "./components/QuizService";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+
+  useEffect(() => {
+    async function fetchQuizQuestion() {
+      try {
+        const fetchedQuestion = await fetchQuestion(9, 5);
+        setQuestions(fetchedQuestion);
+      } catch (error) {
+        console.error("error fetching:", error);
+      }
+    }
+
+    fetchQuizQuestion();
+  }, []);
+
+  const handleAnswerSelect = (selectedOption: string) => {
+    const currentQuestion = questions[currentQuestionIndex];
+    const isCorrect = selectedOption === currentQuestion.correct_answer;
+    if (isCorrect) {
+      setScore((prevScore) => prevScore + 1);
+    }
+    goToNextQuestion();
+  };
+
+  const goToNextQuestion = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    } else {
+      console.log("Completed");
+    }
+  };
+
+  if (questions.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  const currentQuestion = questions[currentQuestionIndex];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header />
+      <Question
+        question={currentQuestion.question}
+        options={[
+          ...currentQuestion.incorrect_answer,
+          currentQuestion.correct_answer,
+        ]}
+        onSelectOptions={handleAnswerSelect}
+      />
+      <Timer duration={10} onTimeUp={goToNextQuestion} />
+      <Score score={score} />
+    </div>
+  );
+};
 
-export default App
+export default App;
